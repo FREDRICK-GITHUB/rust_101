4d1caaeebe0a08369d0571f9c0c2fbabebac6be7
FREDRICK-GITHUB <kamaufredm@gmail.com>
2022-09-19 08:52:38 +0300

    simple program to show how scope works. No scope shadowing is implemented in the program

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,12 @@
-use std::io;
-
 fn main() {
-   loop {
-    println!("Enter A: ");
-    let a: f64 = {
-        let mut number = String::new();
-        io::stdin().read_line( &mut number).expect("error");
-        let nn:f64 = match number.trim().parse(){
-            Ok(num) => num,
-            Err(_) => 0.0,
-        };
-        nn
+    let mut counter = 0; //We are creating a variable with a scope within main function. This variable's value can change but memory address remains the same
+
+    counter = loop{                 // We are creating a loop 
+        counter = counter + 1;      //We are incrementing the value assigned to the variable by 1
+        println!("{}", counter);    //We are printing the current value for the variable
+        if counter == 10{           //Checking the current value of the variable
+            break counter *  2;     //Breaking the loop
+        }
     };
-    println!("\n print B: ");
-    let b = {
-        let mut number = String::new();
-        io::stdin().read_line( &mut number).expect("error");
-        let nn:f64 = match number.trim().parse(){
-            Ok(num) => num,
-            Err(_) => 0.0,
-        };
-        nn
-    };
-    println!("\n use one of these: + - * /");
-    let sigs = {
-        let mut number = String::new();
-        io::stdin().read_line(&mut number).expect("error");
-        let nn:f64 = match number.trim().parse(){
-            Ok(num) => num,
-            Err(_) => 0.0,
-        };
-        nn
-    };
-   }
+    println!("The result is {}", counter); //Printing the current value of the variable after the last condition within the loop scope
 }
